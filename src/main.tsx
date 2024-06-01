5d94f7b612bb4d843f1f87815ea1fe1ac5066889
sina <cna_fp@yahoo.com>
2024-06-01 18:49:14 +0330

    update: package json

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
 import { StoreProvider } from "./store/StoreContext.tsx";
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
-
-
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
